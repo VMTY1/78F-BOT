414fe2d43232a007794bd54894b0ba042a6f69c9
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-07-20 19:27:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1878,8 +1878,8 @@ async def auto_filter(client, msg, spoll=False):
      
     pre = 'filep' if settings['file_secure'] else 'file'
     key = f"{message.chat.id}-{message.id}"
-    FRESH[key] = search
-    temp.GETALL[key] = files
+#    FRESH[key] = search
+#    temp.GETALL[key] = files
     temp.SHORT[message.from_user.id] = message.chat.id 
     search, files, offset, total_results = spoll
     settings = await get_settings(message.chat.id)
